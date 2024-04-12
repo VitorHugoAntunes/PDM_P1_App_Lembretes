6dbde7a8cf29f821faa6c7dd420748e714faeb4d
VitorHugoAntunes <vitor4ntunes@gmail.com>
2024-04-12 19:49:48 -0300

    feat(p1-pdmn): cria componente principal e faz hello

---
+++ README.md
@@ -0,0 +1,13 @@
+# App de lembretes - P1 PDM Noturno
+
+## Integrantes
+
+- Jonathan Ray Menezes Baliellas - 2040482213008
+
+- Kayky Angulo Freitas - 2040482213007
+
+- Paulo Cidrão Gomes Torres - 2040482213010
+
+- Samuel Texeira Dias - 2040482223042
+
+- Vitor Hugo Antunes Passos - 2040482213028
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-6">
+          <h1 className='text-center'>Hello, lembretes</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
