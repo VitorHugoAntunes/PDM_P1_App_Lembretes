8279a4c0e589c98529006c12fc07d670c39c7ebf
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2024-04-18 18:58:03 -0300

    feat(p1-pdmn): adiciona mensagem quando não há tarefas

--- src/components/LembreteLista.tsx
+++ src/components/LembreteLista.tsx
@@ -11,16 +11,27 @@ interface LembreteListaProps {
 export const LembreteLista = ({ tarefas, removerTarefa } : LembreteListaProps) => {
   return (
     <div className="mb-2">
-      <ul className="card list-group rounded border p-2">
-        {tarefas.map((tarefa) => (
-          <li 
-            className="card-body list-group-item text-center m-2 rounded border p-2" 
-            key= {tarefa.id}
-            onClick={() => removerTarefa(tarefa.id)}>
-            {tarefa.titulo}
-          </li>
-        ))}
-      </ul>
+      {
+        tarefas.length === 0 ? (
+          <div className="card rouded border p-2 text-center">
+            <p className="fs-1">🎉</p>
+            <p>Que ótimo! Você não tem tarefas a fazer!</p>
+          </div>
+        )
+        :
+        (
+          <ul className="card list-group rounded border p-2">
+            {tarefas.map((tarefa) => (
+              <li 
+                className="card-body list-group-item text-center m-2 rounded border p-2" 
+                key= {tarefa.id}
+                onClick={() => removerTarefa(tarefa.id)}>
+                {tarefa.titulo}
+              </li>
+            ))}
+          </ul>
+        )
+      }
     </div>
   );
 };
